4580648a472f35e2bbdfb319e557dcf3d6f75a63
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-10-11 16:15:25 -0500

    cambio en DLL, se cambian relaciones entre tablas y se mejora la lógica de negocio

--- BBDD/DDL/DDL_Manzanas_Cuidado.sql
+++ BBDD/DDL/DDL_Manzanas_Cuidado.sql
@@ -1,19 +1,18 @@
--- Active: 1726703214074@@127.0.0.1@3306
 CREATE DATABASE BBDD_Manzanas_Cuidado
     DEFAULT CHARACTER SET = 'utf8mb4';
 
 USE BBDD_Manzanas_Cuidado;
 
+-- Tabla TipoDocumento
+CREATE TABLE Tipo_Documento(
+    TiD_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
+    TiD_Nombre VARCHAR(10)
+);
+
 -- Tabla Manzana
 CREATE TABLE Manzana(
     Man_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
     Man_Nombre VARCHAR(30)
-);
-
--- Tabla Solicitud
-CREATE TABLE Solicitud(
-    Sol_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
-    Sol_Establecimiento VARCHAR(30)
 );
 
 -- Tabla Servicio
@@ -23,9 +22,11 @@ CREATE TABLE Servicio(
     Ser_Descripcion VARCHAR(80)
 );
 
--- Tabla intermedia Manzana_Servicio
-CREATE TABLE Manzana_Servicio(
-    MaS_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
+-- Tabla intermedia Solicitud
+CREATE TABLE Solicitud(
+    Sol_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
+    Sol_Establecimiento VARCHAR(30),
+    Sol_Fecha DATE,
     Man_ID INT(5),
     Ser_ID INT(5),
     FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID),
@@ -35,14 +36,15 @@ CREATE TABLE Manzana_Servicio(
 -- Tabla Usuario
 CREATE TABLE Usuario(
     Usu_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
-    Usu_Tipo_Documento VARCHAR(20),
+    TiD_ID INT(5),
+    Usu_Documento VARCHAR(20),
     Usu_Nombre VARCHAR(20),
-    Usu_Nombre_Usuario VARCHAR(15),
     Usu_Contrasena VARCHAR(30),
     Usu_Telefono VARCHAR(20),
     Usu_Email VARCHAR(30),
     Man_ID INT(5),
     Sol_ID INT(5),
+    FOREIGN KEY(TiD_ID) REFERENCES Tipo_Documento(TiD_ID),
     FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID),
     FOREIGN KEY(Sol_ID) REFERENCES Solicitud(Sol_ID)
 );
