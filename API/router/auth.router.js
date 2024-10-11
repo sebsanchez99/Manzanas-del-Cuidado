141fd26152cba55a6659897f6dc38f94f2b858ae
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-10-11 16:18:34 -0500

    implementación de estrategia local predefinida en ruta de autenticación de usuario

--- API/router/auth.router.js
+++ API/router/auth.router.js
@@ -1,16 +1,12 @@
 const { Router } = require('express')
+const passport = require('passport')
 
 const router = Router()
 
 // http://localhost:3000/auth/login
-// Autentica el usuario con la base de datos
-// TODO: ¿Cómo implementar cookies de autenticación al iniciar sesión?
-router.post('/login', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+// Autentica el usuario con la base de datos usando estrategia local de Passport
+router.post('/login', passport.authenticate('local', { session: false, failureRedirect: '/public/login' }), (req, res) => {
+    res.redirect('/public/inicio')
 })
 
 module.exports = router
