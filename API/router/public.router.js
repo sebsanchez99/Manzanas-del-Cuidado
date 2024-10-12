05951abef68f8d9b6656517706cb3cc6abbfa600
felipe1034 <felipecastanoalvarez6@gmail.com>
2024-10-11 21:18:42 -0500

    logica de registro

--- API/router/public.router.js
+++ API/router/public.router.js
@@ -4,8 +4,8 @@ const path = require('path')
 const router = Router()
 
 // Ruta de inicio de sesión
-router.get('/login', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Login.html'))})
+router.post('/login', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Login.html'))})
 // Ruta de inicio de aplicación 
-router.get('/inicio', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/inicioEjemplo.html'))})
+router.post('/inicio', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/inicioEjemplo.html'))})
 
 module.exports = router
--- API/router/user.router.js
+++ API/router/user.router.js
@@ -1,15 +1,46 @@
-const { Router } = require('express')
-
-const router = Router()
+const { Router } = require("express");
+const MySQL = require("../Libs/mysql");
+const router = Router();
 
 // http://localhost:3000/user/register
 // Registra el usuario en la base de datos
-router.post('/register', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+router.post("/register", async (req, res) => {
+  try {
+    const mySQL = new MySQL()
+    const {
+      TiD_ID,
+      Usu_Documento,
+      Usu_Nombre,
+      Usu_Contrasena,
+      Usu_Telefono,
+      Usu_Email,
+    } = req.body
+    //Confirmar si el usuario existe
+    const query = "SELECT * FROM Usuario WHERE Usu_Email = ?"
+    const results = await mySQL.executeQuery(query, [Usu_Email])
+    //En caso de que el usuario exista
+    if (results.length > 0) {
+      res.status(400).send(`
+    <script>
+        window.onload=function(){
+            alert("Usuario ya existe")
+            window.location.href='./index.html'
+        }
+    </script>
+    `);
     }
-})
+    const registro = 'INSERT INTO Usuario (TiD_ID, Usu_Documento, Usu_Nombre, Usu_Contrasena, Usu_Telefono, Usu_Email) VALUES (?, ?, ?, ?, ?, ?)'
+    const resultadoRegistro = await mySQL.executeQuery(registro, [TiD_ID, Usu_Documento, Usu_Nombre, Usu_Contrasena, Usu_Telefono, Usu_Email])
+    //(Pendiente) validar datos
+    
+    if(resultadoRegistro.affectedRows == 1){
+      res.redirect('/public/login')
+    }
 
-module.exports = router
+  } catch (error) {
+    console.log(error)
+    res.status(500)
+  }
+});
+
+module.exports = router;
