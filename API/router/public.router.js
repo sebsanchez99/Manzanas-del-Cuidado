2e1a972a3400d74b5f3df501787644814a67f70f
felipe1034 <felipecastanoalvarez6@gmail.com>
2024-10-17 21:24:39 -0500

    Se cambio ruta para vista de usuario

--- API/router/public.router.js
+++ API/router/public.router.js
@@ -6,6 +6,6 @@ const router = Router()
 // Ruta de inicio de sesión
 router.post('/login', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Login.html'))})
 // Ruta de inicio de aplicación 
-router.post('/inicio', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/inicioEjemplo.html'))})
+router.post('/inicio', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/usuario.html'))})
 
 module.exports = router
