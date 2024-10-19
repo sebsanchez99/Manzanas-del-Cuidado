d8f663363daaa74d4d8b21b3a7b198cb400ca55a
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-10-19 15:15:02 -0500

    se corrigen archivo DML y DDL

--- BBDD/DDL/DDL_Manzanas_Cuidado.sql
+++ BBDD/DDL/DDL_Manzanas_Cuidado.sql
@@ -22,14 +22,20 @@ CREATE TABLE Servicio(
     Ser_Descripcion VARCHAR(80)
 );
 
+-- Tabla intemedia que relaciona los servicios que contiene cada manzana
+CREATE TABLE Manzana_Servicio(
+    MaS INT(5) PRIMARY KEY AUTO_INCREMENT,
+    Man_ID INT(5),
+    Ser_ID INT(5),
+    FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID),
+    FOREIGN KEY(Ser_ID) REFERENCES Servicio(Ser_ID)
+);
 -- Tabla intermedia Solicitud
 CREATE TABLE Solicitud(
     Sol_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
     Sol_Establecimiento VARCHAR(30),
     Sol_Fecha DATE,
-    Man_ID INT(5),
     Ser_ID INT(5),
-    FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID),
     FOREIGN KEY(Ser_ID) REFERENCES Servicio(Ser_ID)
 );
 
@@ -40,9 +46,10 @@ CREATE TABLE Usuario(
     Usu_Documento VARCHAR(20),
     Usu_Nombre VARCHAR(20),
     Usu_Contrasena VARCHAR(30),
-    Usu_Telefono VARCHAR(20),
     Usu_Email VARCHAR(30),
     Sol_ID INT(5),
+    Man_ID INT(5),  
     FOREIGN KEY(TiD_ID) REFERENCES Tipo_Documento(TiD_ID),
-    FOREIGN KEY(Sol_ID) REFERENCES Solicitud(Sol_ID)
+    FOREIGN KEY(Sol_ID) REFERENCES Solicitud(Sol_ID),
+    FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID)
 );
--- BBDD/DML/DML_Manzanas_Cuidado.sql
+++ BBDD/DML/DML_Manzanas_Cuidado.sql
@@ -1,10 +1,10 @@
 USE BBDD_Manzanas_Cuidado;
 
--- Insertar datos en tabla Tipo_Documento
+-- Insertar datos en la tabla Tipo_Documento
 INSERT INTO Tipo_Documento(TiD_Nombre) VALUES
 ('Cédula de ciudadanía'),
-('Pasaporte'),
-('Cédula de extranjería')
+('Cédula de extranjería'),
+('Pasaporte');
 
 -- Insertar datos en la tabla Manzana
 INSERT INTO Manzana (Man_Nombre) VALUES
@@ -22,18 +22,34 @@ INSERT INTO Servicio (Ser_Nombre, Ser_Descripcion) VALUES
 ('Lavandería Comunitaria', 'Servicio gratuito para lavar la ropa de las cuidadoras y sus familias'),
 ('Asesoría Psicológica', 'Atención y apoyo psicológico para el bienestar mental');
 
+-- Insertar datos en la tabla intermedia Manzana_Servicio
+INSERT INTO Manzana_Servicio (Man_ID, Ser_ID) VALUES
+(1, 1),   
+(1, 3),   
+(2, 2),  
+(2, 5),   
+(3, 1),  
+(3, 4),   
+(4, 2),   
+(4, 3),   
+(5, 4),   
+(5, 5);   
+
 -- Insertar datos en la tabla Solicitud
-INSERT INTO Solicitud (Sol_Establecimiento, Sol_Fecha, Man_ID, Ser_ID) VALUES
-('Centro Comunitario Puente Aranda', '2024-01-15', 1, 1),
-('Centro Comunitario Suba', '2024-02-20', 2, 2),
-('Centro Comunitario Fontibón', '2024-03-10', 3, 3),
-('Centro Comunitario San Cristobal', '2024-04-05', 4, 4),
-('Centro Comunitario Kennedy', '2024-05-18', 5, 5);
+INSERT INTO Solicitud (Sol_Establecimiento, Sol_Fecha, Ser_ID) VALUES
+('Centro Comunitario Puente Aranda', '2024-01-15', 1),
+('Centro Comunitario Suba', '2024-02-20', 2),
+('Centro Comunitario Fontibón', '2024-03-10', 3),
+('Centro Comunitario San Cristobal', '2024-04-05', 4),
+('Centro Comunitario Kennedy', '2024-05-18', 5);
 
 -- Insertar datos en la tabla Usuario
-INSERT INTO Usuario (TiD_ID, Usu_Documento, Usu_Nombre, Usu_Contrasena, Usu_Telefono, Usu_Email, Sol_ID) VALUES
-(1, '123456789', 'Ana Gómez', 'password123', '3001234567', 'ana.gomez@example.com', 1),
-(2, 'A98765432', 'John Smith', 'password456', '3019876543', 'john.smith@example.com', 2),
-(3, 'E76543210', 'Laura Martínez', 'password789', '3027654321', 'laura.martinez@example.com', 3),
-(1, '234567890', 'Carlos Pérez', 'passwordabc', '3002345678', 'carlos.perez@example.com', 4),
-(2, 'B87654321', 'Sofía Rodríguez', 'passworddef', '3018765432', 'sofia.rodriguez@example.com', 5);
+INSERT INTO Usuario (TiD_ID, Usu_Documento, Usu_Nombre, Usu_Contrasena, Usu_Email, Sol_ID, Man_ID) VALUES
+(1, '123456789', 'Ana Gómez', 'password123', 'ana.gomez@example.com', 1, 1),
+(2, 'A98765432', 'John Smith', 'password456', 'john.smith@example.com', 2, 2),
+(3, 'E76543210', 'Laura Martínez', 'password789', 'laura.martinez@example.com', 3, 3),
+(1, '234567890', 'Carlos Pérez', 'passwordabc', 'carlos.perez@example.com', 4, 4),
+(2, 'B87654321', 'Sofía Rodríguez', 'passworddef', 'sofia.rodriguez@example.com', 5, 5);
+
+
+SELECT * FROM USUARIO
