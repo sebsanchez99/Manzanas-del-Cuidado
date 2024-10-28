590b1f446e8abdbb9608dea63622c2fa0d34afbc
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-10-27 22:21:24 -0500

    creación de vista de administración de usuarios

--- API/router/public.router.js
+++ API/router/public.router.js
@@ -10,5 +10,7 @@ router.get('/login', (req, res) => { res.sendFile(path.join(__dirname, '../../Pu
 router.get('/registro', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Registrarse.html'))})
 // Ruta de inicio de aplicación 
 router.get('/inicio', isAuthenticated, (req, res) => { res.sendFile(path.join(__dirname, '../../Public/usuario.html')) })
+//Ruta de administración de usuarios
+router.get('/usuarios', isAuthenticated, (req, res) => { res.sendFile(paht.join(__dirname,'../../Public/listado-usuarios.html')) })
 
 module.exports = router
