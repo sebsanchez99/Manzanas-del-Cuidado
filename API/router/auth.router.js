4ef231384589c15d9eb20f849dc65f7d90153679
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-11-06 21:03:50 -0500

    implementación de lógica de cierre de sesión en API

--- API/router/auth.router.js
+++ API/router/auth.router.js
@@ -7,4 +7,18 @@ const router = Router()
 // Autentica el usuario con la base de datos usando estrategia local de Passport
 router.post('/login', passport.authenticate('local', { failureRedirect: '/public/login', successRedirect: '/public/inicio' }))
 
+// http://localhost:3000/auth/logout
+// Cierra la sesión del usuario y quema la cookie del usuario
+router.post('/logout', (req, res) => {
+    req.logout(function(err) {
+        if (err) {
+            return res.status(500).json({ message: 'Error al cerrar sesión' })
+        }
+        req.session.destroy(() => {
+            res.clearCookie('connect.sid')
+            res.status(200).json({ message: 'Sesión cerrada exitosamente' })
+        })
+    })
+})
+
 module.exports = router
