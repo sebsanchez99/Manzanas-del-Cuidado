fe49e25e928ac1d71bb5db06d02bb3ea6863f15a
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-11-11 21:41:22 -0500

    cambio de ruta que sirve vista inicial

--- API/router/public.router.js
+++ API/router/public.router.js
@@ -9,7 +9,7 @@ router.get('/login', (req, res) => { res.sendFile(path.join(__dirname, '../../Pu
 // Ruta de registro
 router.get('/registro', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Registrarse.html'))})
 // Ruta de inicio de aplicación 
-router.get('/inicio', isAuthenticated, (req, res) => { res.sendFile(path.join(__dirname, '../../Public/usuario.html')) })
+router.get('/inicio', isAuthenticated, (req, res) => { res.sendFile(path.join(__dirname, '../../Public/inicio.html')) })
 //Ruta de administración de usuarios
 router.get('/usuarios', isAuthenticated, (req, res) => { res.sendFile(paht.join(__dirname,'../../Public/listado-usuarios.html')) })
 
