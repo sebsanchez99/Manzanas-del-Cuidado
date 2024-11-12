30acb072cd4c30b5cf50439ee937dc72e4c4efd5
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-11-11 21:40:07 -0500

    cración de nuevo DDL y DML

--- BBDD/DDL/DDL_Manzanas_Cuidado.sql
+++ BBDD/DDL/DDL_Manzanas_Cuidado.sql
@@ -22,21 +22,13 @@ CREATE TABLE Servicio(
     Ser_Descripcion VARCHAR(80)
 );
 
--- Tabla intemedia que relaciona los servicios que contiene cada manzana
+-- Tabla intermedia que relaciona los servicios que contiene cada manzana
 CREATE TABLE Manzana_Servicio(
     MaS INT(5) PRIMARY KEY AUTO_INCREMENT,
     Man_ID INT(5),
     Ser_ID INT(5),
-    FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID),
-    FOREIGN KEY(Ser_ID) REFERENCES Servicio(Ser_ID)
-);
--- Tabla intermedia Solicitud
-CREATE TABLE Solicitud(
-    Sol_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
-    Sol_Establecimiento VARCHAR(30),
-    Sol_Fecha DATE,
-    Ser_ID INT(5),
-    FOREIGN KEY(Ser_ID) REFERENCES Servicio(Ser_ID)
+    FOREIGN KEY (Man_ID) REFERENCES Manzana(Man_ID),
+    FOREIGN KEY (Ser_ID) REFERENCES Servicio(Ser_ID)
 );
 
 -- Tabla Usuario
@@ -47,12 +39,32 @@ CREATE TABLE Usuario(
     Usu_Nombre VARCHAR(20),
     Usu_Contrasena VARCHAR(30),
     Usu_Email VARCHAR(30),
-    Sol_ID INT(5),
     Man_ID INT(5),  
-    FOREIGN KEY(TiD_ID) REFERENCES Tipo_Documento(TiD_ID),
-    FOREIGN KEY(Sol_ID) REFERENCES Solicitud(Sol_ID),
-    FOREIGN KEY(Man_ID) REFERENCES Manzana(Man_ID)
+    FOREIGN KEY (TiD_ID) REFERENCES Tipo_Documento(TiD_ID),
+    FOREIGN KEY (Man_ID) REFERENCES Manzana(Man_ID)
+);
+
+-- Nueva tabla intermedia para asociar Usuarios y Servicios con Fecha
+CREATE TABLE Usuario_Servicio(
+    UsuSer_ID INT(5) PRIMARY KEY AUTO_INCREMENT,
+    Usu_ID INT(5),
+    Ser_ID INT(5),
+    Fecha DATE,
+    FOREIGN KEY (Usu_ID) REFERENCES Usuario(Usu_ID),
+    FOREIGN KEY (Ser_ID) REFERENCES Servicio(Ser_ID)
 );
 
 
-SELECT s.Ser_ID, s.Ser_Nombre, s.Ser_Descripcion, so.Sol_Fecha FROM Servicio s JOIN Solicitud so ON s.Ser_ID = so.Ser_ID JOIN Usuario u ON u.Sol_ID = so.Sol_ID WHERE Usu_ID = 1
+-- Consulta modificada para obtener los servicios de una solicitud específica
+SELECT s.Ser_ID, s.Ser_Nombre, s.Ser_Descripcion, us.Fecha
+FROM Servicio s
+JOIN Usuario_Servicio us ON s.Ser_ID = us.Ser_ID
+JOIN Usuario u ON u.Usu_ID = us.Usu_ID
+WHERE u.Usu_ID = 1;
+
+
+SELECT s.Ser_ID, s.Ser_Nombre, s.Ser_Descripcion 
+FROM Servicio s 
+JOIN Manzana_Servicio ms ON s.Ser_ID = ms.Ser_ID 
+JOIN Manzana m ON m.Man_ID = ms.Man_ID 
+WHERE m.Man_ID = ?
--- BBDD/DML/DML_Manzanas_Cuidado.sql
+++ BBDD/DML/DML_Manzanas_Cuidado.sql
@@ -26,6 +26,8 @@ INSERT INTO Servicio (Ser_Nombre, Ser_Descripcion) VALUES
 INSERT INTO Manzana_Servicio (Man_ID, Ser_ID) VALUES
 (1, 1),   
 (1, 3),   
+(1, 4),   
+(1, 5),   
 (2, 2),  
 (2, 5),   
 (3, 1),  
@@ -35,21 +37,11 @@ INSERT INTO Manzana_Servicio (Man_ID, Ser_ID) VALUES
 (5, 4),   
 (5, 5);   
 
--- Insertar datos en la tabla Solicitud
-INSERT INTO Solicitud (Sol_Establecimiento, Sol_Fecha, Ser_ID) VALUES
-('Centro Comunitario Puente Aranda', '2024-01-15', 1),
-('Centro Comunitario Suba', '2024-02-20', 2),
-('Centro Comunitario Fontibón', '2024-03-10', 3),
-('Centro Comunitario San Cristobal', '2024-04-05', 4),
-('Centro Comunitario Kennedy', '2024-05-18', 5);
-
 -- Insertar datos en la tabla Usuario
-INSERT INTO Usuario (TiD_ID, Usu_Documento, Usu_Nombre, Usu_Contrasena, Usu_Email, Sol_ID, Man_ID) VALUES
-(1, '123456789', 'Ana Gómez', 'password123', 'ana.gomez@example.com', 1, 1),
-(2, 'A98765432', 'John Smith', 'password456', 'john.smith@example.com', 2, 2),
-(3, 'E76543210', 'Laura Martínez', 'password789', 'laura.martinez@example.com', 3, 3),
-(1, '234567890', 'Carlos Pérez', 'passwordabc', 'carlos.perez@example.com', 4, 4),
-(2, 'B87654321', 'Sofía Rodríguez', 'passworddef', 'sofia.rodriguez@example.com', 5, 5);
-
-
-SELECT * FROM USUARIO
+INSERT INTO Usuario (TiD_ID, Usu_Documento, Usu_Nombre, Usu_Contrasena, Usu_Email, Man_ID) VALUES
+(1, '123456789', 'Ana Gómez', 'password123', 'ana.gomez@example.com', 1),
+(2, 'A98765432', 'John Smith', 'password456', 'john.smith@example.com', 2),
+(3, 'E76543210', 'Laura Martínez', 'password789', 'laura.martinez@example.com', 3),
+(1, '234567890', 'Carlos Pérez', 'passwordabc', 'carlos.perez@example.com', 4),
+(2, 'B87654321', 'Sofía Rodríguez', 'passworddef', 'sofia.rodriguez@example.com', 5);
+ delete FROM Usuario_servicio
