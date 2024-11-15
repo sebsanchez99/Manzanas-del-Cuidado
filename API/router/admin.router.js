06a0edf296727e5cbed58966f552f26f118c6646
Aryel Bernaola <aryelbernaolacaballero@gmail.com>
2024-11-14 20:31:35 -0500

    creación de endpoint que actualiza registros de usuarios

--- API/router/admin.router.js
+++ API/router/admin.router.js
@@ -26,11 +26,34 @@ router.delete('/deleteUser', async (req, res) => {
         const query = 'DELETE FROM Usuario WHERE Usu_ID = ?'
         const result = await mySQL.executeQuery(query, [userID]).finally(() => mySQL.closeConnection())
         if (result.affectedRows == 0) req.status(500)
-        res.status(200).json({ message: 'Usuaro eliminado correctamente' })
+        res.status(200).json({ message: 'Usuario eliminado correctamente' })
     } catch (error) {
         console.error(error);
         res.status(500)        
     }
 })
 
+// http://localhost:3000/admin/editUser
+// Actualiza los registros de un usuario
+router.put('/editUser', async (req, res) => {
+    try {
+        const { userID, userName, userEmail,  } = req.body
+        const mySQL = new MySQL()
+        const query = 'UPDATE Usuario SET Usu_Nombre= ?, Usu_Email=? WHERE Usu_ID=?'
+        const result = await mySQL.executeQuery(query, [userName, userEmail, userID]).finally(() => mySQL.closeConnection())
+        if (result.affectedRows == 0) req.status(500)
+        res.status(200).json({ message: 'Usuario actualizado correctamente' })
+    } catch (error) {
+        console.error(error);
+        res.status(500)    
+    }
+})
+
+
+router.post('/ceateService', async (req, res) => {
+    try {
+    } catch (error) {
+        
+    }
+})
 module.exports = router
