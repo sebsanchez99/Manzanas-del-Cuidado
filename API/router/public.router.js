faa0d1f70e1ab53be5bf713b20385cdab36a68c4
Nikolas2407 <nicolasmejianarvaez@gmail.com>
2024-11-16 19:18:17 -0500

    Creacion de formularios y reestructuracion

--- API/router/public.router.js
+++ API/router/public.router.js
@@ -9,8 +9,8 @@ router.get('/login', (req, res) => { res.sendFile(path.join(__dirname, '../../Pu
 // Ruta de registro
 router.get('/registro', (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Registrarse.html'))})
 // Ruta de inicio de aplicación 
-router.get('/inicio', isAuthenticated, (req, res) => { res.sendFile(path.join(__dirname, '../../Public/inicio.html')) })
+router.get('/inicio', isAuthenticated, (req, res) => { res.sendFile(path.join(__dirname, '../../Public/Views/Inicio.html')) })
 //Ruta de administración de usuarios
-router.get('/usuarios', isAuthenticated, (req, res) => { res.sendFile(paht.join(__dirname,'../../Public/listado-usuarios.html')) })
+router.get('/usuarios', isAuthenticated, (req, res) => { res.sendFile(paht.join(__dirname,'../../Public/Usuarios.html')) })
 
 module.exports = router
