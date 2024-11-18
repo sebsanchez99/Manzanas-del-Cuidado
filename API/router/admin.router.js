fc90ee7e97cf3a5c3747824a6fe76b2c034c0f6b
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-11-17 22:29:02 -0500

    creación de endpoint para crear manzana y eliminar manzana

--- API/router/admin.router.js
+++ API/router/admin.router.js
@@ -10,9 +10,9 @@ router.get('/getUsers', async (req, res) => {
         const mySQL = new MySQL()
         const query = 'SELECT Usu_ID, Usu_Nombre, Usu_Email FROM Usuario'
         const users = await mySQL.executeQuery(query).finally(() => mySQL.closeConnection())
-        res.json(users)
+        res.status(200).json(users)
     } catch (error) {
-        console.error(error);
+        console.error(error)
         res.status(500)
     }
 })
@@ -28,7 +28,7 @@ router.delete('/deleteUser', async (req, res) => {
         if (result.affectedRows == 0) req.status(500)
         res.status(200).json({ message: 'Usuario eliminado correctamente' })
     } catch (error) {
-        console.error(error);
+        console.error(error)
         res.status(500)        
     }
 })
@@ -44,16 +44,52 @@ router.put('/editUser', async (req, res) => {
         if (result.affectedRows == 0) req.status(500)
         res.status(200).json({ message: 'Usuario actualizado correctamente' })
     } catch (error) {
-        console.error(error);
+        console.error(error)
         res.status(500)    
     }
 })
 
-
-router.post('/ceateService', async (req, res) => {
+// http://localhost:3000/admin/crearManzana
+// Crea una manzana 
+router.post('/crearManzana', async (req, res) => {
     try {
+        const { nombreManzana, servicios } = req.body
+        const mySQL = new MySQL()
+        const query = 'INSERT INTO Manzana(Man_Nombre) VALUES(?)'
+        const { insertId } = await mySQL.executeQuery(query, [nombreManzana])
+        const insertPromises = servicios.map(idServicio => {
+            const queryServicio = 'INSERT INTO Manzana_Servicio(Man_ID, Ser_ID) VALUES(?, ?)'
+            return mySQL.executeQuery(queryServicio, [insertId, idServicio])
+        })
+        const results = await Promise.all(insertPromises)
+
+        if (results.some(result => result.affectedRows === 0)) {
+            return res.status(500).json({ message: 'Error al crear manzana' })
+        }
+        await mySQL.closeConnection()
+        res.status(200).json({ message: 'Operación exitosa' })
     } catch (error) {
-        
+        console.error(error)
+        res.status(500).json({ message: 'Error al crear manzana' })          
     }
 })
+
+// http://localhost:3000/admin/deleteManzana
+// Crea una manzana 
+router.delete('/deleteManzana', async (req, res) => {
+    try {
+        const { Man_ID} = req.body
+        const mySQL = new MySQL()
+        const queryServicio = 'DELETE FROM Manzana_Servicio WHERE Man_ID = ?'
+        const queryManzana = 'DELETE FROM Manzana WHERE Man_ID = ?'
+        await mySQL.executeQuery(queryServicio, [Man_ID])
+        await mySQL.executeQuery(queryManzana, [Man_ID]).finally(() => mySQL.closeConnection())
+        res.status(200).json({ message: 'Manzana eliminada con éxito' })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Error al crear manzana' })          
+    }
+})
+
+
 module.exports = router
