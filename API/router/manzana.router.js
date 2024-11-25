96d4e0779dea8b6f720b42e2302e883178538c4f
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-11-24 20:08:12 -0500

    creación de endpoints para traer información de manzana y actualizar manzana

--- API/router/manzana.router.js
+++ API/router/manzana.router.js
@@ -16,15 +16,56 @@ router.get('/getManzanasInfo', async (req, res) => {
     }
 })
 
+// http://localhost:3000/manzana/getManzanasServices
+// Obtiene la información de una manzana en específico con los servicios que tiene y los que no
+router.get('/getManzanasServices', async (req, res) => {
+    try {
+        const { Man_ID } = req.body
+        const mySQL = new MySQL()
+        const manzanaQuery = 'SELECT * FROM Manzana WHERE Man_ID = ?'
+        const manzana = await mySQL.executeQuery(manzanaQuery, [Man_ID])
+        const serviciosQuery = `
+        SELECT 
+            s.Ser_ID,
+            s.Ser_Nombre,
+            CASE WHEN ms.Man_ID IS NOT NULL THEN true ELSE false END AS checked
+        FROM Servicio s
+        LEFT JOIN Manzana_Servicio ms ON s.Ser_ID = ms.Ser_ID AND ms.Man_ID = ?
+        ORDER BY s.Ser_ID
+        `
+        const serviciosArray = await mySQL.executeQuery(serviciosQuery, [Man_ID]).finally(() => mySQL.closeConnection())
+        const servicios = serviciosArray.map(servicio => ({
+            ...servicio,
+            checked: servicio.checked == 1
+        }))
+        res.status(200).json({
+            manzanaId: manzana.ManID,
+            manzanaNombre: manzana.Man_Nombre,
+            servicios
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500)        
+    }
+})
+
 // http://localhost:3000/manzana/updateManzana
+// Actualiza una manzana
 router.put('/updateManzana', async (req, res) =>{
     try {
-        const { Man_Nombre, ManID} = req.body
+        const { manzanaID, manzanaName, servicios} = req.body
         const mySQL = new MySQL()
-        const query = 'UPDATE Manzana SET Man_Nombre=? WHERE Man_ID=?'
-        const result = await mySQL.executeQuery(query, [Man_Nombre, ManID]).finally(() => mySQL.closeConnection)
-        if (result.affectedRows == 0) req.status(500)
-            res.status(200).json({ message: 'Manzana actualizada correctamente' }) 
+        const updateQuery = 'UPDATE Manzana SET Man_Nombre=? WHERE Man_ID=?'
+        await mySQL.executeQuery(updateQuery, [manzanaName, manzanaID])
+        const deleteQuery = 'DELETE FROM Manzana_Servicio WHERE Man_ID = ?'
+        await mySQL.executeQuery(deleteQuery, [manzanaID])
+        const insertQuery = 'INSERT INTO ManzanaServicio(Man_ID, Ser_ID) VALUES(?, ?)'
+        const insertPromises = servicios
+            .filter(servicio => servicio.checked)
+            .map(servicio => mySQL.executeQuery(insertQuery, [manzanaID, servicio.id]))
+        await Promise.all(insertPromises)
+        await mySQL.closeConnection()
+        res.status(200).json({ message: 'Manzana actualizada con éxito' });
     } catch (error) {
         console.error(error)
         res.status(500)   
